8c5afd428e39dd457d4f118f429c5f9f3e40041d
vladimir-bezrukov <web.dev903@gmail.com>
2021-01-10 02:27:52 +0200

    perf(frontend): dynamic viewerView and signinLink

--- src/frontend/components/Header/index.tsx
+++ src/frontend/components/Header/index.tsx
@@ -1,11 +1,17 @@
 import { useSelector } from 'react-redux';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import dynamic from 'next/dynamic';
+
 import { AppState } from 'frontend/redux/root-reducer';
 import { ThemeModeSwitch } from './ThemeModeSwitch';
-import { ViewerView } from './ViewerView';
 import { HomeLink } from './HomeLink';
-import { SignInLink } from './SignInLink';
+const ViewerView = dynamic<Record<string, unknown>>(() =>
+  import('./ViewerView').then((mod) => mod.ViewerView),
+);
+const SignInLink = dynamic<Record<string, unknown>>(() =>
+  import('./SignInLink').then((mod) => mod.SignInLink),
+);
 
 export const Header: React.FC = () => {
   const { viewer } = useSelector((state: AppState) => state.auth);
